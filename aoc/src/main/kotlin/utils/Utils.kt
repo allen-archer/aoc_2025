c541b401a0ed346057ef84a07bda07c29c66fc9c
allen <chunkystyles@gmail.com>
2025-12-01 10:40:17 -0500

    AoC 2025 setup

---
+++ aoc/src/main/kotlin/utils/Utils.kt
@@ -0,0 +1,93 @@
+package utils
+
+import java.net.HttpURLConnection
+import java.net.URI
+import java.nio.file.Paths
+import kotlin.io.path.name
+
+private const val INPUT_URL_TEMPLATE = "https://adventofcode.com/2025/day/{day}/input"
+private var inputIoStartTime = 0L
+private var inputParseStartTime = 0L
+private var algorithmStartTime = 0L
+
+/**
+ * Retrieves the puzzle input for a given day from a local file, and if not present, then from the Advent of Code website.
+ *
+ * @param day The day of the Advent of Code challenge.
+ * @return The puzzle input as a String.
+ * @throws Exception If there is an error reading from the file and also fetching from the website.
+ */
+fun getInput(day: Int): String {
+    val dayString = day.toString()
+    val fileName = "input_${dayString.padStart(2, '0')}.txt"
+    return try {
+        readFileText(fileName)
+    } catch (e: Exception) {
+        val input = getInputFromSite(dayString)
+        writeTextToFile(fileName, input)
+        input
+    }
+}
+
+/**
+ * Sets the start time for the file IO phase.
+ */
+fun setInputIoStartTime() {
+    inputIoStartTime = System.currentTimeMillis()
+}
+
+/**
+ * Sets the start time for the input parsing phase.
+ */
+fun setInputParseStartTime() {
+    inputParseStartTime = System.currentTimeMillis()
+}
+
+/**
+ * Sets the start time for the algorithm phase.
+ */
+fun setAlgorithmStartTime() {
+    algorithmStartTime = System.currentTimeMillis()
+}
+
+/**
+ * Calculates run time of each phase of the program.
+ *
+ * @return The formatted run time of each phase of the program.
+ */
+fun getElapsedTime(): String {
+    val now = System.currentTimeMillis()
+    return if (inputParseStartTime == 0L || algorithmStartTime == 0L) {
+        "Elapsed time: ${getElapsedTimeBetween(inputIoStartTime, now)}"
+    } else {
+        "Input IO: ${getElapsedTimeBetween(inputIoStartTime, inputParseStartTime)}\n" +
+                "Input Parse: ${getElapsedTimeBetween(inputParseStartTime, algorithmStartTime)}\n" +
+                "Algorithm: ${getElapsedTimeBetween(algorithmStartTime, now)}\n" +
+                "Total: ${getElapsedTimeBetween(inputIoStartTime, now)}"
+    }
+}
+
+private fun getElapsedTimeBetween(start: Long, end: Long): String {
+    val elapsedTime = end - start
+    val minutes = elapsedTime / 60000
+    val seconds = (elapsedTime % 60000) / 1000
+    val milliseconds = elapsedTime % 1000
+    return "${minutes}m ${seconds}s ${milliseconds}ms"
+}
+
+private fun getInputFromSite(day: String): String {
+    val uri = URI(INPUT_URL_TEMPLATE.replace("{day}", day))
+    val connection = uri.toURL().openConnection() as HttpURLConnection
+    connection.requestMethod = "GET"
+    connection.setRequestProperty("Cookie", readFileText("/home/allen/repos/aoc_2025/aoc/cookie"))
+    connection.connect()
+    return connection.inputStream.bufferedReader().use { it.readText() }
+}
+
+fun readFileText(fileName: String): String {
+    return Paths.get(fileName).toFile().readText()
+}
+
+fun writeTextToFile(fileName: String, text: String) {
+    Paths.get(fileName).toFile().writeText(text)
+}
