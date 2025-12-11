7adc219185a7667c1d9274f161a0890d9d1d4ab3
allen <chunkystyles@gmail.com>
2025-12-11 10:03:36 -0500

    Day 11 part 1

---
+++ aoc/src/main/kotlin/day11/Part1.kt
@@ -0,0 +1,38 @@
+package day11
+
+import utils.*
+
+const val end = "out"
+var map: Map<String, List<String>>? = null
+var visited: Set<String> = mutableSetOf()
+
+fun main() {
+    setInputIoStartTime()
+    val input = getInput(11)
+    setInputParseStartTime()
+    map = parseInput1(input)
+    setAlgorithmStartTime()
+    val count = countPaths("you")
+    println(getElapsedTime())
+    println(count)
+}
+
+fun countPaths(vertex: String): Int {
+    val otherVertices = map!![vertex]!!
+    var total = 0
+    for (otherVertex in otherVertices) {
+        if (otherVertex == end) {
+            total++
+        } else if (!visited.contains(otherVertex)) {
+            total += countPaths(otherVertex)
+        }
+    }
+    return total
+}
+
+fun parseInput1(input: String): Map<String, List<String>> = input.lines()
+    .filter { it.isNotBlank() }
+    .associate {
+        val split = it.split(": ")
+        split[0] to split[1].split(" ").toList()
+    }
